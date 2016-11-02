6ada276b2c1ca110c0925a28c9b37e30e3e96a20
Dastardly Vandal <dast@localhost.localdomain>
2016-11-01 21:44:08 -0700

    Complete "Complete Daily Goal"

--- app/controllers/recurring_goals_controller.rb
+++ app/controllers/recurring_goals_controller.rb
@@ -1,6 +1,18 @@
 class RecurringGoalsController < ApplicationController
+
+  def show
+    RecurringGoal.find(params[:id]).update completed: true
+    redirect_to :back
+  end
+
   def create
     current_user.recurring_goals.create(name: params[:recurring_goal][:name], completed: false)
     redirect_to :back
   end
+
+# For quick reference when working with resque
+#  def resetGoals
+#    RecurringGoal.update_all(completed: "false")
+#  end
+
 end
---
+++ lib/resetGoals.rb
@@ -0,0 +1,1 @@
+RecurringGoal.update_all(completed: "false")
