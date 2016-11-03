3928c825be6281ac94adff86b1e319ad512f9663
Dastardly Vandal <dast@localhost.localdomain>
2016-11-03 13:56:16 -0700

    Added case where if user changed the url to see another user, they get redirected to the home page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_filter :validate_user
+
   def show
     @user = current_user
     @recurring_goal = RecurringGoal.new
   end
+
+  private
+    def validate_user
+      redirect_to(root_url) unless current_user.id.to_s == params[:id]
+    end
 end
