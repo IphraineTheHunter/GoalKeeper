a1c9b47e4ed63ca2c471fc7913ae76e3b8317739
IphraineTheHunter <stmcalpine@hotmail.com>
2016-11-05 23:32:51 -0700

    make the mini-goal type buttons appear selected when you click on them

--- app/assets/javascripts/recurring_goals.js
+++ app/assets/javascripts/recurring_goals.js
@@ -2,12 +2,14 @@ window.onload = function(){
 	var dailyButton = $(".daily.recurring-goals");
 	$(".daily.select-goal-type").click(function(){switchToGoalType('daily');});
 	$(".weekly.select-goal-type").click(function(){switchToGoalType('weekly');});
-	$(".monthly.select-goal-type").click(function(){switchToGoalType('montly');});
+	$(".monthly.select-goal-type").click(function(){switchToGoalType('monthly');});
 
 	
 	
 	function switchToGoalType(goalType){
 		$('.recurring-goals').addClass('hidden');
+		$('.select-goal-type').removeClass('selected');
 		$('.' + goalType).removeClass('hidden');
+		$('.select-goal-type.' + goalType).addClass('selected');
 	}
 };
