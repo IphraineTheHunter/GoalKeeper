19947f75698f5280c04244073014acfdc971207e
Dastardly Vandal <dast@localhost.localdomain>
2016-11-08 12:18:10 -0800

    Added routes and html to show the user their Milestone Tracks on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
-    
+      @user = current_user
+      @milestone_track = MilestoneTrack.new
+      @milestone = Milestone.new
+      @milestones_all = Milestone.all
   end
 end
